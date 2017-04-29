3dc9288093c35b877da56eff7c2cb50b763d3727
Aaron Parker <aaron@stealthpuppy.com>
2017-04-29 20:22:43 +1000

    Working Install-VisualCRedistributables.ps1; Updated VisualCRedistributables.xml; Added VisualCRedistributables.xsd

--- Microsoft Visual C++ Redistributables/Install-VisualCRedistributables.ps1
+++ Microsoft Visual C++ Redistributables/Install-VisualCRedistributables.ps1
@@ -24,25 +24,28 @@
         .\Install-VisualCRedistributables.ps1 -Xml ".\VisualCRedistributables.xml"
 
         Description:
-        Specifies the path to the XMl file containing the registry values to set and file actions and records to a log file at $env:SystemRoot\Temp\Set-Customisations.log.
+        Downloads and installs the Visual C++ Redistributables listed in VisualCRedistributables.xml.
 #>
 
 [CmdletBinding(SupportsShouldProcess = $True, ConfirmImpact = "Low")]
 PARAM (
     [Parameter(Mandatory=$True, HelpMessage="The path to the XML document describing the Redistributables.")]
-    # [ValidateScript({Test-Path $_ -PathType 'Container'})]
-    [string]$Xml,
+    [ValidateScript({ Test-Path $_ -PathType 'Leaf' })]
+    [string]$File,
 
-    [Parameter(Mandatory=$False, HelpMessage="Specify to download the Redistributables and not install them.")]
-    [bool]$DownloadOnly = $False
+    [Parameter(Mandatory=$False, HelpMessage="Specify a target path to download the Redistributables to.")]
+    [string]$Path = ".\",
+
+    [Parameter(Mandatory=$False, HelpMessage="Enable the installation of the Redistributables after download.")]
+    [bool]$Install = $False    
 )
 
 BEGIN {
     # Variables
-    $build = [Environment]::OSVersion.Version
+    # $build = [Environment]::OSVersion.Version
 
     # Get script elevation status
-    [bool]$Elevated = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+    # [bool]$Elevated = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
 }
 
 PROCESS {
@@ -56,40 +59,36 @@ PROCESS {
         Return $?
     }
 
-    # Read the specifed XML document
-    $xmlDocument = ( Select-Xml -Path $Xml -XPath / ).Node
+    # Read the specifed XML document    
+    $xmlContent = ( Select-Xml -Path $File -XPath "/Redistributables/Platform" ).Node
 
-    # Select settings from the document for the current OS
-    $customisations = $xmlDocument.SelectNodes("/Redistributables")
+    # Loop through each setting in the XML structure to set the registry value
+    ForEach ( $platform in $xmlContent ) {
 
-    # Testing
-    # ForEach ( $setting in $customisations.SelectNodes("DefaultUser/Registry/*[@Path]") ) {
-    #     Write-Host "$default\$($setting.Path)"
-    #     ForEach ( $value in $setting.SelectNodes("Values/*[@Name]") ) {
-    #         Write-Host "Value: $($value.Name); Data: $($value.InnerText)"
-    #     }
-    # }
-
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>> Default Profile
-        # Set defaut profile
-        # Loop through each setting in the XML structure to set the registry value
-        ForEach ( $platform in $customisations.SelectNodes("Platform") ) {
+        # Create variables from the content to simplify references below
+        $plat = $platform | Select-Object -ExpandProperty Architecture
+        $rel = $platform | Select-Object -ExpandProperty Release
+        $arg = $platform | Select-Object -ExpandProperty Install
+        
+        ForEach ($redistributable in $platform.Redistributable ) {
             
-            # Set specified registry values specified in the XML
-            ForEach ( $value in $setting.SelectNodes("Values/*[@Name]") ) {
-                If ($pscmdlet.ShouldProcess("$($value.Name) in $default\$($setting.Path)", "Create value")) {
+            # Create variables from the content to simplify references below
+            $uri = $redistributable.Download
+            $filename = $uri.Substring($uri.LastIndexOf("/") + 1)
+            $target= "$((Get-Item $Path).FullName)\$rel\$plat\$($redistributable.Name)"
 
-                    # Convert values to an integer where required (fix with an xsd?)
-                    If ($value.Data.Type -eq "Int") {  
-                        $val = $value.InnerText.ToInt32($Null)
-                    } Else {
-                        $val = $value.InnerText
-                    }
-
-                    # Add or set the registry value
-                    New-ItemProperty -Path "$default\$($setting.Path)" -Name $($value.Name) -Value $val -Force
+            # Create the folder to store the downloaded file
+            If (!(Test-Path -Path $target)) {
+                If ($pscmdlet.ShouldProcess($target, "Create")) {
+                    New-Item -Path $target -Type Directory -Force
                 }
             }
+
+            # Download the Redistributable to the target path
+            If ($pscmdlet.ShouldProcess("$uri", "Download")) {
+                Invoke-WebRequest -Uri $uri -OutFile "$target\$filename"
+            }
         }
+    }
 
 }
